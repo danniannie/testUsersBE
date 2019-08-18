174133a732584e01e19f31052040cb7ce7b55cac
danniannie <dannielle.young@hotmail.com>
2019-08-18 18:28:54 +0100

    first commit

---
+++ functions/index.js
@@ -0,0 +1,49 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.get("/users", (req, res, next) => {
+  return db
+    .collection("userList")
+    .get()
+    .then(snapshot => res.send(snapshot.docs.map(doc => doc.data())));
+});
+
+app.post("/users", (req, res, next) => {
+  const body = req.body;
+  return db
+    .collection("userList")
+    .doc(body.email)
+    .set(body)
+    .then(documentSnapshot => {
+      let data = documentSnapshot.data();
+      return res.status(200).send({ data });
+    });
+});
+
+app.get("/users/:userID", (req, res, next) => {
+  return db
+    .collection("userList")
+    .doc(req.params.userID)
+    .get()
+    .then(snapshot => res.status(200).send(snapshot));
+});
+
+app.del("/users/:userID", (req, res, next) => {
+  return db
+    .collection("userList")
+    .doc(req.params.userID)
+    .delete()
+    .then(() => res.status(200).send({ message: "user deleted" }));
+});
+
+//   .patch(updateUser)
+
+exports.app = functions.https.onRequest(app);
---
+++ functions/serviceKey.js
@@ -0,0 +1,14 @@
+exports.serviceAccount = {
+  type: "service_account",
+  project_id: "ttest-ba0bc",
+  private_key_id: "6bf76f0e2585a06bde18ce2c9ea31ffa54f89131",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCUcAg1KveC7GHy\nFWoVuGvKBk81ykp4mGoMLJX9xlrl1rKp2b/VcihogmF2VsTiGrTww4RK0ZQpa/rk\nJeFUfp/InVaVM+9ji7IdGkoV5rSSRrtEtW54UKTcHnIZ03kGImKZTHlQhvSSrpcB\nZrP6svsrGbV18l2b/DJzhc6YCzG9eEMvUwRN631Itex+ZTh5nH7bNLJYSiBx7/37\nEQ+WvnK++8/3C1cTp9NEFYIU3SMybSpBZeC12RHY5qLzg6NrffZ2Mhf4UhgC3Jt6\nRGS8Jz5J1K52u2QD46KMVt5X5ecjfc4RkbGOOfJqY2BiLy1ivIkBb3Vffoy123SL\nh7z1K7HHAgMBAAECggEAF8grSHlt1mgQ+Tw9FuXJP0Drh+61J8UjmHe8QEB7Z7+S\nOGxOs9/qYXQiMVeNi7xDqR9qHqKdXlWSiOMYyvTOnNG4gYckn4pOKyN/IysnWxhC\nAKwg2t3wy+OE3GZpSXZQ3cbG2QQGQ0n9n5fY1h2+6BTiYddG8Crj2mZhxzJj8bsI\nXnA2tbGbuNrMNRUXBSE0vIZy074AqUBOdBknKRm5+1bGTivvr0KaT6TIXwQhyWlX\n+plX6WVjgQTshxBnEqfuUjUMdlYdd6z8RUjf8itAfTUHYR7MQJCvyF/iUMJm8lTL\nLjcmyhhKWuap8lRTwIkQKVmYZBiVVmgFly0ygmrkoQKBgQDMwYbEa/Zl6uQas6o1\nXxaem+YcYM4MpVztvdyMQCD0FmV9hTftN6DWYD5ralNkh8MZQs6q8kcNv15xOdaU\nGT6kZudr6X3G7xj06vGoNKzwMlh4ggakg20RzWOiKLbTTqoE/BhXiEamVBE0DUEl\nZf2pcH1Vz5mEKXn5fVCnWAOJJQKBgQC5lkGK7fHvnT8FMJx8E5ffU86NhroXt+A2\nQ3TMnw3EXLH6jXfLHD1eKftd9gJctxIwGyIF0B6GoBq8HKPXK6b2650Dvy/ldvvT\nXY7g7cy+0W0UjOM7wz57Edp++PFzGCADInxAu/PGG/PY08C6ujboesPaxD2+v1pD\nlYKf0ISJewKBgEMecTn0FgokrG9gU7w5FmYZvp2w0fptPoAkS6ah4uuAZ/Z0c/fL\n02pSlEOL0BhVNIwUTUDWOJp2D9RX3FIh0Fn97YB/Qp6yg+DNHzg/RC1co4mlPd4x\nwtAiHrXqWLv9vlfx91zCLD6bsiP48FD9FOC1IjC945niKryw6n5gqJANAoGBAKy0\ng3ICVK8jqk3uGvHsWdzeZM2Xnv4Lg3977ums6mbdzFLZ66z8Lq++acna8f/N7gMO\nfO7j6kRmIfsEvAO0/7+xK4EdHjlwLJFZ82imY27//X6uTZiD7VUiHC8lA6yTutav\n9OlY/1ZRFO+mvygFuqtF85VWIg+sixdxvwlyRqSZAoGAHbsUbPuShQbh9Zd3Shhw\nd/kfobHpSwa51nBkyDIOXobkoUMDniWVe28PqnA190QgGj+DJitH7niBDptmETkW\n1MisGJMYsLTg8pcypmvSTkUhKc1XkKgBkOjsdxZOUCGrdqAtLDcANGYjjYWZoeP0\nsEGDiY9dZJ3IcDu6IiCriLs=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-6upv8@ttest-ba0bc.iam.gserviceaccount.com",
+  client_id: "116219300733384396766",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-6upv8%40ttest-ba0bc.iam.gserviceaccount.com"
+};
